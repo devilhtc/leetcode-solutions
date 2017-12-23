0764f0ba2266e1203b98cdd89e87ddc66c55d186
devilhtc <devilmengsk@gmail.com>
2017-12-23 10:31:25 -0800

    changed format to hex to index

--- addProblem.py
+++ addProblem.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+HEX_LEN = 4
+
 def hello(a,b):
     print("hello and that's your sum:", a + b)
 
@@ -12,10 +14,17 @@ def createDir(dir):
 	if not os.path.exists(dir):
 		os.makedirs(dir)
 
+
+def toHexString(num, hexlen):
+	hexStr = hex(num)
+	hexStr = hexStr[:2] + '0' * (hexlen + 2 -len(hexStr)) +  hexStr[2:]
+	return hexStr
+
+
 def createProblem(args, verbose = False):
 	args = [s.lower() for s in args]
 	args = [s.title() for s in args]
-	dirname = args[0]+'.'+'_'.join(args[1:])
+	dirname = toHexString(int(float(args[0])), HEX_LEN) + '_' + args[0]+'.'+'_'.join(args[1:])
 	
 	if verbose:
 		print('creating directory: ' + dirname)
