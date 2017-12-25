65e21f8ea3d680652954a7616d6a5b06ffcb3535
devilhtc <devilmengsk@gmail.com>
2017-12-24 23:45:37 -0800

    added 15, 16

---
+++ 0x0010_16.3Sum_Closest/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums = sorted(nums)
+        if len(nums) < 3: return sum(nums)
+        out = sum(nums[:3])
+        minDis = abs(out - target)
+        for i in range(len(nums) - 2):
+            
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                curSum = nums[i] + nums[l] + nums[r]
+                curDis = abs(target - curSum)
+                if curSum == target:
+                    return target
+                if curDis < minDis:
+                    minDis = curDis
+                    out = curSum
+                if curSum > target:
+                    r -= 1
+                else:
+                    l += 1
+        return out
