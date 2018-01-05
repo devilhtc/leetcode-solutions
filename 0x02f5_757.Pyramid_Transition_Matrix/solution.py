dfcc8976444b161dfec774141d95ee8a1d7b84d9
devilhtc <devilmengsk@gmail.com>
2018-01-05 11:17:41 -0800

    modified 757

--- 0x02f5_757.Pyramid_Transition_Matrix/solution.py
+++ 0x02f5_757.Pyramid_Transition_Matrix/solution.py
@@ -1,34 +1,5 @@
-# their solution: accepted but rong (problematic when T[A[i], A[i+1]] = [])
+# now accepted solution (they decreased test size)
 
-class Solution(object):
-    def pyramidTransition(self, bottom, allowed):
-        T = collections.defaultdict(set)
-        for u, v, w in allowed:
-            T[u, v].add(w)
-
-        #Comments can be used to cache intermediate results
-        seen = set()
-        def solve(A):
-            if len(A) == 1: return True
-            if A in seen: return False
-            seen.add(A)
-            return any(solve(cand) for cand in build(A, []))
-
-        def build(A, ans, i = 0):
-            if i + 1 == len(A):
-                yield "".join(ans)
-            else:
-                for w in T[A[i], A[i+1]]:
-                    ans.append(w)
-                    for result in build(A, ans, i+1):
-                        yield result
-                    ans.pop()
-
-        return solve(bottom)
-
-# my solution: TLE but correct
-
-'''
 def getPyra(bottom):
     pyra = []
     for i in range(len(bottom) - 1):
@@ -91,5 +62,3 @@ class Solution(object):
         self.memo[state] = ret
         #print pyra, ret
         return ret
-
-'''
