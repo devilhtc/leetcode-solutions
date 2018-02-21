c4ea34c005657d490c2a82a14554ea106b3a6871
devilhtc <devilmengsk@gmail.com>
2018-02-21 00:06:30 -0800

    added 781 :

---
+++ 0x030d_781.Rabbits_In_Forest/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numRabbits(self, answers):
+        """
+        :type answers: List[int]
+        :rtype: int
+        """
+        count = 0
+        d = {}
+        for a in answers:
+            d[a] = d.get(a, 0) + 1
+            if d[a] == a + 1:
+                count += (a+1)
+                d[a] = 0
+        for k in d:
+            if d[k] != 0:
+                count += (k+1)
+        return count
+        
