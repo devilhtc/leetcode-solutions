1e3f41dc12a1a0a07312bce48e880837fea10a21
devilhtc <devilmengsk@gmail.com>
2018-02-21 00:07:27 -0800

    added 779

---
+++ 0x030b_779.K-Th_Symbol_In_Grammar/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kthGrammar(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        if N == 1: return 0
+        return (self.kthGrammar(N - 1, (K + 1)/2) + K + 1) % 2
+        
