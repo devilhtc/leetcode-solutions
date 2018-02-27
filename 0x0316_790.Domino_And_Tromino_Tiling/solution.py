520703ca19f7a777450f377e42d87bf73ca4ef7a
devilhtc <devilmengsk@gmail.com>
2018-02-27 09:55:05 -0800

    added 790 dp

---
+++ 0x0316_790.Domino_And_Tromino_Tiling/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numTilings(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        l = max(3, N + 1)
+        
+        MOD = 1000000007
+        
+        f = [0 for _ in range(l)]
+        g = [0 for _ in range(l)]
+        f[0] = 1
+        f[1] = 1
+        f[2] = 2
+        g[2] = 1
+        
+        for i in range(3, l):
+            f[i] = (2*g[i-1] + f[i-1] + f[i-2]) % MOD
+            g[i] = (f[i-2] + g[i-1]) % MOD
+            
+        return f[N]
+        
