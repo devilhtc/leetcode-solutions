a170e9c94e43d7449b84ab3d834a98e396e6b344
devilhtc <devilmengsk@gmail.com>
2018-03-11 23:14:14 -0700

    added 797

---
+++ 0x031d_797.All_Paths_From_Source_To_Target/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        out = []
+        stack = []
+        def dfs(node):
+            stack.append(node)
+            if node == len(graph) - 1:
+                out.append(list(stack))
+            else:
+                children = graph[node]
+                for c in children:
+                    dfs(c)
+            stack.pop()
+        dfs(0)
+        return out
+            
+        
