c3b36a4fd40b9258995bc15e6198c0c23a8939ea
devilhtc <devilmengsk@gmail.com>
2018-05-06 17:19:37 -0700

    added 831

---
+++ 0x033f_831.Masking_Personal_Information/solution.py
@@ -0,0 +1,23 @@
+import re
+
+def generateMaskedNumbers(l):
+    return ('+' if l>6 else '') + '*' * (l-6) + ('-' if l>6 else '') + '***-***-'
+
+class Solution(object):
+    def maskPII(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        if '@' in S:
+            return self.maskEmail(S)
+        else:
+            return self.maskPhone(S)
+    
+    def maskEmail(self, e):
+        name1, name23 = tuple(e.split('@'))
+        return name1[0].lower() + '*****' + name1[-1].lower() + '@' + name23.lower()
+    
+    def maskPhone(self, p):
+        allNumbers = ''.join([n for n in p if re.search('[0-9]', n) is not None])
+        return generateMaskedNumbers(len(allNumbers) - 4) + allNumbers[-4:]
