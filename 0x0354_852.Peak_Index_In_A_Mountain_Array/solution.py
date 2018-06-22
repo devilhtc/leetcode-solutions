043b04464815e9af9ad98b5104d325b9c53aa881
devilhtc <devilmengsk@gmail.com>
2018-06-21 23:51:43 -0700

    added 852

---
+++ 0x0354_852.Peak_Index_In_A_Mountain_Array/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        mi = 0 # maximum index
+        for i in range(len(A)):
+            if A[i] > A[mi]:
+                mi = i
+        return mi
