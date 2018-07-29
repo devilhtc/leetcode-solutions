3f7cc28b0e9d1cf1454f75b44eaf43b2dd1723a8
devilhtc <devilmengsk@gmail.com>
2018-07-29 16:55:11 -0700

    added 880

---
+++ 0x0370_880.Random_Pick_With_Weight/solution.py
@@ -0,0 +1,39 @@
+import random
+
+class Solution:
+
+    def __init__(self, w):
+        """
+        :type w: List[int]
+        """
+        self.build(w)
+    
+    def build(self, w):
+        self._w = [0.0]
+        total = sum(w)
+        cur = 0.0
+        for i in w:
+            cur += i
+            self._w.append(cur / total)
+        
+    def pickIndex(self):
+        """
+        :rtype: int
+        """
+        return self._pickIndex(random.random())
+    
+    def _pickIndex(self, r):
+        lo = 0
+        hi = len(self._w) - 1
+        while hi - lo > 1:
+            mi = int((hi + lo) / 2)
+            if r < self._w[mi]:
+                hi = mi
+            else:
+                lo = mi
+        return lo
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
