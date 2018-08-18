49b52bc18a1f24e4e1440bb68d524ecb8c03ddc9
devilhtc <devilmengsk@gmail.com>
2018-08-17 22:45:35 -0700

    added 884

---
+++ 0x0374_884.Uncommon_Words_From_Two_Sentences/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uncommonFromSentences(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: List[str]
+        """
+        return (lambda x: [ele for ele in x if x[ele] == 1])(collections.Counter((A + ' ' + B).split()))
+        
