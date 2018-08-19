b0350a363906f1b8ff7e5fdfce524939860f4fc4
devilhtc <devilmengsk@gmail.com>
2018-08-18 22:25:23 -0700

    added 888

---
+++ 0x0378_888.Fair_Candy_Swap/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fairCandySwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        dif = sum(A) - sum(B)
+        sb = set(B)
+        out = [0, 0]
+        for i in A:
+            if i - dif//2 in sb:
+                out = [i, i - dif//2]
+        return out
+        
