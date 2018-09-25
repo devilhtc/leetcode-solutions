ff8ea7378cbbbe9f783b758f26e8795f716c0ceb
devilhtc <devilmengsk@gmail.com>
2018-09-24 18:10:56 -0700

    oops add 909 again

--- 0x038d_909.Snakes_And_Ladders/solution.py
+++ 0x038d_909.Snakes_And_Ladders/solution.py
@@ -1,18 +1,39 @@
 class Solution:
-    def smallestRangeII(self, A, K):
+    def snakesAndLadders(self, board):
         """
-        :type A: List[int]
-        :type K: int
+        :type board: List[List[int]]
         :rtype: int
         """
-        if len(A) == 0:
-            return 0
-        A = sorted(A)
-        r = A[-1] - A[0]
-        for i in range(len(A) - 1):
-            curmin = min(A[0] + K, A[i+1] - K)
-            curmax = max(A[-1] - K, A[i] + K)
-            if curmax - curmin < r:
-                r = curmax - curmin
-        return r
+        N = len(board)
+        
+        jumps = {}
+        for i in range(N):
+            for j in range(N):
+                n = (N - i - 1) * N + ((j + 1) if (N - i - 1) % 2 == 0 else (N - j))
+                if board[i][j] != -1:
+                    jumps[n] = board[i][j]
+        
+        board_step = [N * N + 1] * (N * N)
+        board_step[0] = 0
+        queue = [0]
+        c = 0
+        
+        while c < len(queue):
+            i = queue[c]
+            c = c + 1
+            n = i + 1
+            def step(x):
+                if board_step[x] > board_step[i] + 1:
+                    board_step[x] = board_step[i] + 1
+                    queue.append(x)
+                
+            for j in range(i + 1, i + 7):
+                if j >= N * N:
+                    continue
+                if j + 1 in jumps:
+                    k = jumps[j + 1] - 1
+                    step(k)
+                else:
+                    step(j)
 
+        return -1 if board_step[-1] == N * N + 1 else board_step[-1]
