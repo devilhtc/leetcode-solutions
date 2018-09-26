b4bb9c05dd3e1d77a831d97bdcd06ea302375b15
devilhtc <devilmengsk@gmail.com>
2018-09-26 01:14:50 -0700

    added 880

---
+++ 0x0370_880.Decoded_String_At_Index/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def decodeAtIndex(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        target = K
+        stack = [('', 0)]
+        for c in S:
+            if not c.isdigit():
+                stack.append((c, stack[-1][1] + 1))
+            else:
+                stack.append((c, stack[-1][1] * int(c)))
+            if stack[-1][1] >= K:
+                break
+        while True:
+            if len(stack) == 0:
+                break
+            if not stack[-1][0].isdigit():
+                if stack[-1][1] == target:
+                    return stack[-1][0]
+                else:
+                    stack.pop()
+            else:
+                target = ((target - 1) % stack[-2][1]) + 1
+                stack.pop()
+        return ''
+        
