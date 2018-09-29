4058d50dcfe649efa733661af5e8583d1c8e9e48
devilhtc <devilmengsk@gmail.com>
2018-09-28 20:15:43 -0700

    added 867

---
+++ 0x0363_867.Transpose_Matrix/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def transpose(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n = len(A), len(A[0])
+        return [[A[j][i] for j in range(m)] for i in range(n)]
