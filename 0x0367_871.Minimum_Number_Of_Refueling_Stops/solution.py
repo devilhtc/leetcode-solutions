16382b54e00d9beae127e06e0318ac05588ff5c1
devilhtc <devilmengsk@gmail.com>
2018-10-02 17:34:05 -0700

    added 871 hard

---
+++ 0x0367_871.Minimum_Number_Of_Refueling_Stops/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minRefuelStops(self, target, startFuel, stations):
+        pq = []
+        res = i = 0
+        cur = startFuel
+        while cur < target:
+            while i < len(stations) and stations[i][0] <= cur:
+                heapq.heappush(pq, -stations[i][1])
+                i += 1
+            if not pq: return -1
+            cur += -heapq.heappop(pq)
+            res += 1
+        return res
