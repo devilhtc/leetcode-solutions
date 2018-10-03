c210062041851b3c5dfab7d7c3f43f5b1923793b
devilhtc <devilmengsk@gmail.com>
2018-10-03 16:52:33 -0700

    added 825

---
+++ 0x0339_825.Friends_Of_Appropriate_Ages/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numFriendRequests(self, ages):
+        """
+        :type ages: List[int]
+        :rtype: int
+        """
+        ages = sorted(ages)
+        last_index = {v: i for i, v in enumerate(ages)}
+        out = 0
+        for i, v in enumerate(ages):
+            ub = v # upper bound is v, inclusive
+            lb = v / 2 + 7 + 0.01  # lower bound, exclusive
+            if lb > ub: # empty interval
+                continue
+            lo, hi = 0, i
+            while hi > lo:
+                mi = (hi + lo) // 2
+                if ages[mi] < lb:
+                    lo = mi + 1
+                else:
+                    hi = mi 
+            out += (last_index[v] - lo)
+        return out
+            
+
+
+
