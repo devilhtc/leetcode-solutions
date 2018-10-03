7a59d7c3a63083fbc70efe297c2349f6640e3cbb
devilhtc <devilmengsk@gmail.com>
2018-10-03 00:46:40 -0700

    added 821

---
+++ 0x0335_821.Shortest_Distance_To_A_Character/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        l = len(S)
+        left = [l + 1] * l
+        right = [l + 1] * l
+        for i, v in enumerate(S):
+            if v == C:
+                left[i] = 0
+            else:
+                left[i] = left[i] if i == 0 else left[i-1] + 1
+        for j, v in reversed(list(enumerate(S))):
+            if v == C:
+                right[j] = 0
+            else:
+                right[j] = right[j] if (j == l - 1) else right[j+1] + 1
+        return [min(left[i], right[i]) for i in range(l)]
+                
+        
