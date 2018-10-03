ae9b52311b07d34c37a22e161789927e29e574bd
devilhtc <devilmengsk@gmail.com>
2018-10-02 20:27:29 -0700

    added 898

---
+++ 0x0382_898.Bitwise_Ors_Of_Subarrays/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subarrayBitwiseORs(self, A):
+        res, cur = set(), set()
+        for i in A:
+            cur = {i | j for j in cur} | {i}
+            res |= cur
+        return len(res)
+        
