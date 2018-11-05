ed774cf56e8a20d5ee85dd7b2677346fb09a5173
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-11-05 11:41:52 -0800

    added 73

---
+++ 0x0049_73.Set_Matrix_Zeroes/solution.py
@@ -0,0 +1,42 @@
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        if m == 0:
+            return
+        n = len(matrix[0])
+        if n == 0:
+            return
+        # (i, 0), (0, j)
+        marks = [1, 1]
+
+        for i in range(m):
+            if matrix[i][0] == 0:
+                marks[0] = 0
+        for j in range(n):
+            if matrix[0][j] == 0:
+                marks[1] = 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            if matrix[i][0] == 0:
+                for j in range(1, n):
+                    matrix[i][j] = 0
+        for j in range(1, n):
+            if matrix[0][j] == 0:
+                for i in range(1, m):
+                    matrix[i][j] = 0
+        if marks[0] == 0:
+            for i in range(m):
+                matrix[i][0] = 0
+        if marks[1] == 0:
+            for j in range(n):
+                matrix[0][j] = 0
