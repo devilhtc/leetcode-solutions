4ea18e32cebbab81966dbe9caec581ec03a705bc
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-11-29 17:28:49 -0800

    added 945, 947

---
+++ 0x03b1_945.Minimum_Increment_To_Make_Array_Unique/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minIncrementForUnique(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        if len(A) == 0:
+            return 0
+        A = sorted(A)
+        e = A[0]
+        out = 0
+        for num in A:
+            if num <= e:
+                out += (e - num)
+                e += 1
+            else:
+                e = num + 1
+        return out
---
+++ 0x03b3_947.Most_Stones_Removed_With_Same_Row_Or_Column/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeStones(self, stones):
+        """
+        :type stones: List[List[int]]
+        :rtype: int
+        """
+        uf = {}
+        Y_OFFSET = 10001
+        def union(i, j):
+            j = j + 10001
+            fi = find(i)
+            fj = find(j)
+            if fi == fj:
+                return
+            if fi > fj:
+                fi, fj = fj, fi
+            uf[fj] = fi
+        
+        def find(i):
+            if i not in uf:
+                uf[i] = i
+            j = i
+            while uf[j] != j:
+                j = uf[j]
+            uf[i] = j
+            return j
+        
+        for s in stones:
+            union(*s)
+        return len(stones) - len(set(find[ele] for ele in uf))
