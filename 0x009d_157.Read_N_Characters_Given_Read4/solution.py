b4c8ae138a2452de61466dca54112fe2711fdbb3
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-12-14 09:34:21 -0800

    added 157

---
+++ 0x009d_157.Read_N_Characters_Given_Read4/solution.py
@@ -0,0 +1,25 @@
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        o = 0
+        temp = [''] * 4
+        while n > 0:
+            c = read4(temp)
+            k = min(n, c)
+            for i in range(k):
+                buf[o + i] = temp[i]
+            o += k
+            n -= 4
+        return o
+                
+            
+        
