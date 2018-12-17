cbbf2c00bfd08fc210101a82922666bbf7d4f11b
devilhtc <devilmengsk@gmail.com>
2018-12-16 23:05:57 -0800

    added 960

---
+++ 0x03c0_960.Delete_Columns_To_Make_Sorted_III/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        m, n = len(A), len(A[0])
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if all(A[k][i] >= A[k][j] for k in range(m)):
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return n - max(dp)        
