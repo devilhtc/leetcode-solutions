387d70615e1c6f460f9710b2daf1d8611e00cb80
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-12-20 13:04:09 -0800

    added 332

---
+++ 0x014c_332.Reconstruct_Itinerary/solution.py
@@ -0,0 +1,20 @@
+# solution: eulerian path from @yangwangx
+class Solution:
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        G = collections.defaultdict(list)
+        for u, v in tickets: G[u].append(v)
+        for u in G: G[u].sort(reverse=True)
+        
+        route = []
+        def dfs(at):
+            while G[at]:
+                to = G[at].pop()
+                dfs(to)
+            route.append(at)
+
+        dfs("JFK")
+        return route[::-1]
