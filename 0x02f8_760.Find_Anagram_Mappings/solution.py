c4c63f2ff709efbb12c3c4f48d185ee9062b7a56
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-12-20 13:12:55 -0800

    added 760, 832

---
+++ 0x02f8_760.Find_Anagram_Mappings/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def anagramMappings(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        dA = collections.defaultdict(list)
+        dB = collections.defaultdict(list)
+        for i in range(len(A)):
+            dA[A[i]].append(i)
+            dB[B[i]].append(i)
+        
+        out = [0] * len(A)
+        for k in dA:
+            ai = dA[k]
+            bi = dB[k]
+            for i in range(len(ai)):
+                out[ai[i]] = bi[i]
+        return out
+            
+        
---
+++ 0x0340_832.Flipping_An_Image/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for r in A:
+            self.processRow(r)
+        
+        return A
+    def processRow(self, row):
+        i = 0
+        j = len(row) - 1
+        while j > i:
+            row[i], row[j] = 1 - row[j], 1 - row[i]
+            j -= 1
+            i += 1
+        if i == j:
+            row[i] = 1 - row[i]
