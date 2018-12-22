90e978a488234dcecb4c2c2b7f8d385f1574ade6
Tianchang (Kevin) He <devilmengsk@gmail.com>
2018-12-22 11:23:07 -0800

    filled 833

---
+++ 0x0341_833.Find_And_Replace_In_String/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findReplaceString(self, S, indexes, sources, targets):
+        """
+        :type S: str
+        :type indexes: List[int]
+        :type sources: List[str]
+        :type targets: List[str]
+        :rtype: str
+        """
+        if len(indexes) == 0:
+            return S
+        out = []
+        z = sorted(list(zip(indexes, sources, targets)))
+        out.append(S[:z[0][0]])
+        
+        for i, v in enumerate(z):
+            idx, sou, tar = v
+            m = True
+            for j in range(len(sou)):
+                if sou[j] != S[idx + j]:
+                    m = False
+                    break
+            nidx = z[i + 1][0] if i < len(z) - 1 else len(S)
+            if m:
+                out.append(tar)
+                out.append(S[idx + len(sou): nidx])
+            else:
+                out.append(S[idx: nidx])
+        
+        return ''.join(out)
