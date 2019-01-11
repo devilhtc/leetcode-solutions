9e1cbca98d7492456e4123cec0d6701563aa3276
Tianchang (Kevin) He <devilmengsk@gmail.com>
2019-01-11 13:20:13 -0800

    added 326

--- 0x0129_297.Serialize_and_Deserialize_Binary_Tree/solution.py
+++ 0x0129_297.Serialize_and_Deserialize_Binary_Tree/solution.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+
 class Codec:
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -12,29 +13,29 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+
         def jsonfy(node):
             if node is None:
                 return None
-            return {
-                'v': node.val,
-                'l': jsonfy(node.left),
-                'r': jsonfy(node.right)
-            }
+            return {"v": node.val, "l": jsonfy(node.left), "r": jsonfy(node.right)}
+
         return json.dumps(jsonfy(root))
-    
+
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
+
         def dejsonfy(o):
             if o == None:
                 return None
-            node = TreeNode(int(o['v']))
-            node.left = dejsonfy(o['l'])
-            node.right = dejsonfy(o['r'])
+            node = TreeNode(int(o["v"]))
+            node.left = dejsonfy(o["l"])
+            node.right = dejsonfy(o["r"])
             return node
+
         return dejsonfy(json.loads(data))
 
 
