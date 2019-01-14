35ad213d8d80dc55f6bdc0596392b1c20d74efaf
Tianchang (Kevin) He <devilmengsk@gmail.com>
2019-01-14 11:29:29 -0800

    added 215 quick select

---
+++ 0x00d7_215.Kth_Largest_Element_in_an_Array/solution.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        # quick select with group of 5
+        g = 5
+
+        def mom(arr):
+            # medium of medium
+            ms = []
+            i = 0
+            while i < len(arr):
+                sub_arr = arr[i : min(len(arr), i + g)]
+                ms.append(sorted(sub_arr)[len(sub_arr) // 2])
+                i += g
+            out = dac(ms, len(ms) // 2)
+            return out
+
+        def dac(arr, o):
+            # divide and conquer
+            if o == 1:
+                return max(arr)
+            elif o == len(arr):
+                return min(arr)
+
+            if len(arr) < 10:
+                return sorted(arr)[-o]
+
+            p = mom(arr)
+            l, m, h = [], [], []
+            for a in arr:
+                if a < p:
+                    l.append(a)
+                elif a == p:
+                    m.append(p)
+                else:
+                    h.append(a)
+
+            if o <= len(h):
+                return dac(h, o)
+            elif len(h) + len(m) >= o > len(h):
+                return p
+            else:
+                return dac(l, o - len(h) - len(m))
+
+        return dac(nums, k)
