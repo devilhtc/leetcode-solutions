5998fe4269ec261fbce5706cc679128a61ef389f
devilhtc <devilmengsk@gmail.com>
2019-01-19 15:30:15 -0800

    added 778

--- 0x030a_778.Swim_In_Rising_Water/solution.py
+++ 0x030a_778.Swim_In_Rising_Water/solution.py
@@ -1,27 +1,50 @@
-import heapq
-
-
-class Solution(object):
+class Solution:
     def swimInWater(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        N = len(grid)
-        visited = [[False] * N for _ in range(N)]
-        visited[0][0] = True
-        boundary = []
-        heapq.heappush(boundary, (grid[0][1], 0, 1))
-        heapq.heappush(boundary, (grid[1][0], 1, 0))
-        visited[1][0], visited[0][1] = True, True
-        for t in range(grid[0][0], N * N):
-            while len(boundary) > 0 and boundary[0][0] <= t:
-                h, i, j = heapq.heappop(boundary)
-                if i == N - 1 and j == N - 1:
-                    return t
-                for ii, jj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-                    if 0 <= ii < N and 0 <= jj < N:
-                        if not visited[ii][jj]:
-                            heapq.heappush(boundary, (grid[ii][jj], ii, jj))
-                            visited[ii][jj] = True
-        return -1
+        n = len(grid)
+        if grid[-1][-1] == n ** 2 - 1:
+            return n ** 2 - 1
+
+        uf = {}
+
+        def find(i):
+            if i not in uf:
+                uf[i] = i
+                return i
+
+            o = i
+            while uf[i] != i:
+                i = uf[i]
+            uf[o] = i
+            return i
+
+        def union(i, j):
+            fi = find(i)
+            fj = find(j)
+            if fi == fj:
+                return
+
+            if fi < fj:
+                uf[fj] = fi
+            else:
+                uf[fi] = fj
+
+        e2k = {}
+        for i in range(n):
+            for j in range(n):
+                e2k[grid[i][j]] = i * n + j
+        deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for w in range(n ** 2):
+            k = e2k[w]
+            i, j = divmod(k, n)
+            for di, dj in deltas:
+                i2, j2 = i + di, j + dj
+                if 0 <= i2 < n and 0 <= j2 < n:
+                    if grid[i2][j2] <= w:
+                        union(k, i2 * n + j2)
+            if find(0) == find(n ** 2 - 1):
+                return w
+        return n ** 2 - 1
